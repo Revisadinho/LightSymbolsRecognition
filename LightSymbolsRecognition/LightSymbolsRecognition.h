c6952a00d12cbe8376dc151db0665ca6f251a89b
jessicaguiot <jessica.guiot97@gmail.com>
2021-10-06 08:43:05 -0300

    Initial Commit

---
+++ LightSymbolsRecognition/LightSymbolsRecognition.h
@@ -0,0 +1,18 @@
+//
+//  LightSymbolsRecognition.h
+//  LightSymbolsRecognition
+//
+//  Created by Jéssica Araujo on 06/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LightSymbolsRecognition.
+FOUNDATION_EXPORT double LightSymbolsRecognitionVersionNumber;
+
+//! Project version string for LightSymbolsRecognition.
+FOUNDATION_EXPORT const unsigned char LightSymbolsRecognitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightSymbolsRecognition/PublicHeader.h>
+
+
